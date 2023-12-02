be0abd882e601af822a565837dfb6e0c54798ca0
SUYASH7707 <suyashbhatt09@gmail.com>
2023-12-02 04:22:12 -0500

    Added comments

--- codes/haar_casscade.py
+++ codes/haar_casscade.py
@@ -9,12 +9,12 @@ def capture_faces(cascade, frame, face_count, save_path, max_faces=100):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
     for (x, y, w, h) in faces:
-        # Increase the height of the rectangle
-        height_increase = 20  # Adjust this value to change the height of the rectangle
+        
+        height_increase = 20  
         y_new = y - height_increase // 2
         h_new = h + height_increase
 
-        # Draw a rectangle with increased height
+        
         cv2.rectangle(frame, (x, y_new), (x + w, y_new + h_new), (255, 0, 0), thickness=3)
 
         face_region = frame[y:y+h, x:x+w]
@@ -29,7 +29,7 @@ def main():
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
     cap = cv2.VideoCapture(0)
 
-    save_path = r'C:\Users\suyas\OneDrive-stevens.edu\Desktop\Applied Machine Learning\Final_Project\images'
+    save_path = r'C:\Users\suyas\OneDrive-stevens.edu\Desktop\AAI695\AAI695\images'
     create_directory(save_path)
     face_count = 0
     max_faces = 100
